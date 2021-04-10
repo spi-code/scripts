b4763baad1d7c553a09d21117974e9688c6c6bc4
spi-code <53261379+spi-code@users.noreply.github.com>
2021-04-10 10:30:59 +0200

    Create raspiconfig.sh

---
+++ raspiconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export DEBIAN_FRONTEND=noninteractive
+apt-get -yq install sudo wget curl unzip jq openjdk-9-jre openjdk-9-jdk vim 
+
+#Docker
+curl -fsSL https://get.docker.com | sh
+sudo usermod -aG docker pi
